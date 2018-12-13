106b6cb5c708649aa211beaec16ab740917cb744
YuriyPrivezentsev <y.privezentsev@gmail.com>
2018-12-14 00:39:56 +0100

    adjust for gygrometer data

--- javascripts/create-graph.js
+++ javascripts/create-graph.js
@@ -2,7 +2,7 @@
  * Parse the data and create a graph with the data.
  */
 function parseData(createGraph) {
-	Papa.parse("../data/spanish-silver.csv", {
+	Papa.parse("../data/data.gigrometer.csv", {
 		download: true,
 		complete: function(results) {
 			createGraph(results.data);
@@ -11,35 +11,34 @@ function parseData(createGraph) {
 }
 
 function createGraph(data) {
-	var years = [];
-	var silverMinted = ["Silver Minted"];
+	var timestamp = ['x'];
+	var humidity = ["Humidity"];
 
-	for (var i = 1; i < data.length; i++) {
-		years.push(data[i][0]);
-		silverMinted.push(data[i][2]);
+	for (var i = 1; i < data.length-1; i++) {
+		timestamp.push(new Date(data[i][0]).toISOString());
+		humidity.push(data[i][2]);
 	}
 
-	console.log(years);
-	console.log(silverMinted);
+	console.log(timestamp);
+	console.log(humidity);
 
 	var chart = c3.generate({
 		bindto: '#chart',
 	    data: {
-	        columns: [
-	        	silverMinted
+    			x: 'x',
+					xFormat: '%Y-%m-%dT%H:%M:%S.%LZ',
+					columns: [
+						timestamp,
+	        	humidity
 	        ]
 	    },
 	    axis: {
-	        x: {
-	            type: 'category',
-	            categories: years,
-	            tick: {
-	            	multiline: false,
-                	culling: {
-                    	max: 15
-                	}
-            	}
-	        }
+				x: {
+            type: 'timeseries',
+            tick: {
+                format: '%d/%m %H:%M'
+            }
+        }
 	    },
 	    zoom: {
         	enabled: true
